64d1dada9ee169007b0e590fcd4218ad4d075d22
Alexander Benavides <fabenavideso@gmail.com>
2020-09-19 21:43:48 -0500

    Read

--- Crud.js
+++ Crud.js
@@ -12,9 +12,9 @@ class Crud {
         this.table = table
     };
     
-    create (table, item) {   
+    create (item) {   
         var params = {
-            TableName:table, Item:item
+            TableName:this.table, Item:item
         };
         
         mDB.put(params, function(err, data) {
@@ -23,9 +23,24 @@ class Crud {
             }else {
               console.log("Added item:", JSON.stringify(data, null, 2));
             }
+        });      
+    }
+
+    Read (key){
+        var params = {
+            TableName:this.table, Key:key
+        };
+
+        console.log(params);
+
+        mDB.get(params, function(err, data) {
+            if (err) {
+                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            }
         });
-      
-      }
+    }
 
 }
 
--- Main.js
+++ Main.js
@@ -4,11 +4,14 @@ var Crud = require("./Crud");
 const table = "Users";
 const mItem = new Item();
 const mCrud = new Crud(table);
+const mKey = new Item();
 
 mItem.setEmail("Juana la cubana");
 mItem.setId(12345);
 
-mCrud.create(table, mItem);
+mCrud.create(mItem);
 
+mKey.setId(12345);
+mCrud.Read(mKey);
 
 
